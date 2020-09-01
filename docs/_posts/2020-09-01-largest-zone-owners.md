22e94c7083d81c979b5b99a6f4242920855caa88
Shane Curcuru <asf@shanecurcuru.org>
2020-09-01 08:42:46 -0400

    New blog post

---
+++ docs/_posts/2020-09-01-largest-zone-owners.md
@@ -0,0 +1,78 @@
+---
+title: Arlington's Top Landowners By Zone
+excerpt: Charting who owns what in different Zoning Areas of Arlington.
+classes: wide
+categories:
+  - Property
+tags:
+  - Property
+  - Assessments
+  - Zoning
+---
+
+While Arlington is primarily a residential town, there are plenty of district or even industrial districts here.  But who owns what kind of land - that is, in which zoning areas?
+
+## Top Land Owners In Arlington
+
+Unlike our [earlier comparison of the top landowners in Arlington](https://arlingtonma.info/property/largest-property-owners/), these charts include the Town of Arlington itself in calculations.  Charts include the top 10 owners by land size in each zoning area.  For example, in zone _B4 - Vehicular Oriented Business_, the Mirak family's various auto properties comprise 13% of all _B4_-zoned land in Arlington by size.  The percentage of **All Others** is then shown in black for all smaller holders combined in each zone's chart.
+
+See [Business districts](#business), [Industrial or Other districts](#industrial), [Residential districts](#residential), or [Data Sources](#data-sources).  Not shown is the Transportation district (MBTA, mainly) or W/WATER areas (like Spy pond or the Res).  Note that the _MU - Multi-Use_ and _PUD - Planned Unit Development_ zones are effectively only single owners.
+
+## Business Districts
+
+<figure class="half" id="business">
+<div class='chartfigure'><h3 style='text-align: center;'>B1 - Neighborhood Office</h3><div id='zoneB1'></div></div>
+<div class='chartfigure'><h3 style='text-align: center;'>B2 - Neighborhood Business</h3><div id='zoneB2'></div></div>
+</figure>
+<figure class="half">
+<div class='chartfigure'><h3 style='text-align: center;'>B2A - Major Business</h3><div id='zoneB2A'></div></div>
+<div class='chartfigure'><h3 style='text-align: center;'>B3 - Village Business</h3><div id='zoneB3'></div></div>
+</figure>
+<figure class="half">
+<div class='chartfigure'><h3 style='text-align: center;'>B4 - Vehicular Oriented Business</h3><div id='zoneB4'></div></div>
+<div class='chartfigure'><h3 style='text-align: center;'>B5 - Central Business</h3><div id='zoneB5'></div></div>
+</figure>
+
+## Industrial Or Other Districts
+
+<figure class="half" id="industrial">
+<div class='chartfigure'><h3 style='text-align: center;'>I - Industrial</h3><div id='zoneI'></div></div>
+<div class='chartfigure'><h3 style='text-align: center;'>MU - Multi-Use</h3><div id='zoneMU'></div></div>
+</figure>
+<figure class="half">
+<div class='chartfigure'><h3 style='text-align: center;'>OS - Open Space</h3><div id='zoneOS'></div></div>
+<div class='chartfigure'><h3 style='text-align: center;'>PUD - Planned Unit Development</h3><div id='zonePUD'></div></div>
+</figure>
+
+# Residential Districts
+
+<figure class="half" id="residential">
+<div class='chartfigure'><h3 style='text-align: center;'>R0 - Large Lot Single Family</h3><div id='zoneR0'></div></div>
+<div class='chartfigure'><h3 style='text-align: center;'>R1 - Single Family</h3><div id='zoneR1'></div></div>
+</figure>
+<figure class="half">
+<div class='chartfigure'><h3 style='text-align: center;'>R2 - Two Family</h3><div id='zoneR2'></div></div>
+<div class='chartfigure'><h3 style='text-align: center;'>R3 - Three Family</h3><div id='zoneR3'></div></div>
+</figure>
+<figure class="half">
+<div class='chartfigure'><h3 style='text-align: center;'>R4 - Town House</h3><div id='zoneR4'></div></div>
+<div class='chartfigure'><h3 style='text-align: center;'>R5 - Apartments Low Density</h3><div id='zoneR5'></div></div>
+</figure>
+<figure class="half">
+<div class='chartfigure'><h3 style='text-align: center;'>R6 - Apartments Med Density</h3><div id='zoneR6'></div></div>
+<div class='chartfigure'><h3 style='text-align: center;'>R7 - Apartments High Density</h3><div id='zoneR7'></div></div>
+</figure>
+
+## Data Sources
+
+All figures are derived from [official data sources from the Town's GIS department](/property) including the most recently _assessed_ value of parcels.  Using a simple [Ruby programming script](https://github.com/ArlingtonMA/arlingtonma.info/blob/master/src/assessorparser.rb), we have analyzed the core ArlingtonMA_Assessor table of all owners of land in town to sum up overall ownership records.  Since many commercial properties are owned by trusts or LLC corporations, we have also consolidated beneficial ownership in selected cases for some major property owners in town.  Entries in _UPPERCASE_ are exact parcel owners directly in Assessor rolls; entries _In Mixed Case_ are beneficial owners of various companies or realty trusts.  Note that our beneficial owner research may be incomplete or inaccurate depending on actual ownership structures.
+
+Property size percentage of a zone is calculated against a summing up of all property LOT_SIZE within a zone.  Note that comparisons against the separate ArlingtonMA_Zoning file don't work (not sure why; we've asked the GIS department for comment).
+
+
+<!-- Actually load our charts/tables -->
+<link href="/assets/css/c3.css" rel="stylesheet">
+<script src="/assets/js/d3.min.js" charset="utf-8"></script>
+<script src="/assets/js/c3.min.js"></script>
+<script src="/assets/js/dataread.js"></script>
+<script src="/assets/js/largest-zone-owners.js"></script>
---
+++ docs/assets/js/largest-zone-owners.js
@@ -0,0 +1,252 @@
+// Generated via assets/code/assessorparser.rb::output_zoneownersjs()
+addDonutChart('#zoneB1', [
+  ['Henry E. Davidson, Jr.', 0.0661],
+  ['UNITED STATES GOVERNMENT', 0.0455],
+  ['NYBERG JONATHAN M &', 0.0378],
+  ['1021 MASSACHUSETTS AVENUE LLC', 0.0321],
+  ['Town Of Arlington', 0.0231],
+  ['U.S. OF AMERICA POST OFFICE', 0.0225],
+  ['SAMPSON  MARTHA /TRUSTEE', 0.0205],
+  ['CAMPOBASSO PROPERTIES LLC', 0.0205],
+  ['58-60 MEDFORD STREET LLC', 0.0186],
+  ['H.A.F. INC.', 0.0186],
+  ['HIGHROCK CHURCH', 0.018],
+  ['All Others', 0.6767]
+], 'Zone B1', { 'All Others': 'black' })
+
+addDonutChart('#zoneB2', [
+  ['CAMPOBASSO PROPERTIES LLC', 0.0656],
+  ['ATINIZIAN NIGOGHOS & CAROLYN', 0.0656],
+  ['KADETS PHILIP L/ELAINE/TRS', 0.0414],
+  ['BLAKE RICHARD A JR TR', 0.0391],
+  ['Frank Pasciuto', 0.038],
+  ['LOCKE PLACE LLC', 0.0311],
+  ['PARTH ENTERPRISES LLC', 0.0288],
+  ['TOULOPOULOS JOHN  W/TRS/ETAL', 0.0276],
+  ['GOBOOK INC', 0.0276],
+  ['THE CHILDRENS CENTER', 0.0265],
+  ['SUMMER REALTY SERVICES LLC', 0.0265],
+  ['All Others', 0.5822]
+], 'Zone B2', { 'All Others': 'black' })
+
+addDonutChart('#zoneB2A', [
+  ['US REIF BRIGHAM SQUARE', 0.2063],
+  ['MARLEY WILLIAM GNC', 0.1237],
+  ['Mirak Family', 0.1226],
+  ['THE STOP&SHOP SUPERMRKT LESSEE', 0.1034],
+  ['DE VINCENT ARTHUR TRS-ETAL', 0.0789],
+  ['DEJESUS JOHN ANDREW TRS', 0.0741],
+  ['NWCA ARLINGTON LLC', 0.073],
+  ['CLAREMONT ARLINGTON SUITES LLC', 0.0634],
+  ['WHEATLEY REAL ESTATE LLC', 0.0299],
+  ['ODONOGHUE JOHN & KEVIN', 0.0187],
+  ['TARLIN LLOYD ETAL TRUSTEES', 0.0176],
+  ['All Others', 0.0884]
+], 'Zone B2A', { 'All Others': 'black' })
+
+addDonutChart('#zoneB3', [
+  ['P&D REALTY', 0.0776],
+  ['Mirak Family', 0.0662],
+  ['SHEFFIELD PARTNERS LLC', 0.058],
+  ['AKBARIAN MOHAMMED MD', 0.0567],
+  ['1398 MASSACHUSETTS AVE. LLC', 0.0517],
+  ['Frank Pasciuto', 0.046],
+  ['STOREY PROPERTIES LLC', 0.0347],
+  ['Town Of Arlington', 0.0277],
+  ['1406 MASS AVE LLC', 0.0259],
+  ['LEVOSKY CAROLYN TR', 0.024],
+  ['TRITON-ARLINGTON LLC', 0.024],
+  ['All Others', 0.5075]
+], 'Zone B3', { 'All Others': 'black' })
+
+addDonutChart('#zoneB4', [
+  ['Mirak Family', 0.1301],
+  ['NOYES REALTY LLLP', 0.098],
+  ['KO NAI NAN ETAL/ TRUSTEES', 0.0396],
+  ['BAYSTATE CONDOMINIUMS LLC', 0.0369],
+  ['864 MASS AVENUE LLC', 0.0353],
+  ['JK HOLDINGS LLC', 0.0273],
+  ['TD BANKNORTH NA', 0.0262],
+  ['BOSTON EDISON CO', 0.0252],
+  ['1420 MASSACHUSETTS AVE LLC', 0.0241],
+  ['ARLINGTON AUTOS LLC', 0.023],
+  ['KALOOSDIAN ROBERT A TRUSTEE', 0.023],
+  ['All Others', 0.5113]
+], 'Zone B4', { 'All Others': 'black' })
+
+addDonutChart('#zoneB5', [
+  ['Mirak Family', 0.3399],
+  ['Town Of Arlington', 0.1533],
+  ['CAMBRIDGE SAVINGS BANK', 0.0766],
+  ['BANK OF AMERICA', 0.0692],
+  ['JOHNS SON LLC', 0.0532],
+  ['LACOURT ENTERPRISES LLC', 0.047],
+  ['450 MASS AVE LLC', 0.042],
+  ['COHEN MAX R TRUSTEE', 0.0408],
+  ['BLUMSACK CHARLES E/ETAL', 0.0371],
+  ['MEGUERDITCHIAN RICHARD', 0.0272],
+  ['GECHIJIAN ARA K', 0.026],
+  ['All Others', 0.0877]
+], 'Zone B5', { 'All Others': 'black' })
+
+addDonutChart('#zoneH', [
+  ['BRIGHTVIEW ARLINGTON LLC', 1.0],
+  ['All Others', 0.0]
+], 'Zone H', { 'All Others': 'black' })
+
+addDonutChart('#zoneI', [
+  ['Mirak Family', 0.2599],
+  ['Town Of Arlington', 0.1805],
+  ['Henry E. Davidson, Jr.', 0.1034],
+  ['ARLINGTON COAL & LUMBER CO', 0.0635],
+  ['NOSTALGIA PROPERTIES LLC', 0.0291],
+  ['LALICATA REALTY LLC', 0.0241],
+  ['SANTINI MARK & GARY--TRS', 0.0229],
+  ['87 MYSTIC STREET LLC', 0.0211],
+  ['FARRELL DANIEL J/TRUSTEE', 0.0209],
+  ['HIGHROCK CHURCH INC', 0.019],
+  ['34 DUDLEY STREET LLC', 0.0177],
+  ['All Others', 0.2379]
+], 'Zone I', { 'All Others': 'black' })
+
+addDonutChart('#zoneMU', [
+  ['FHF 1 ARLINGTON 360 LLC', 1.0],
+  ['All Others', 0.0]
+], 'Zone MU', { 'All Others': 'black' })
+
+addDonutChart('#zoneO', [
+  ['Mugar Family', 1.0],
+  ['All Others', 0.0]
+], 'Zone O', { 'All Others': 'black' })
+
+addDonutChart('#zoneOS', [
+  ['Town Of Arlington', 0.987],
+  ['Mugar Family', 0.0108],
+  ['MASS BAY TRANSIT AUTHORITY', 0.0021],
+  ['All Others', 0.0001]
+], 'Zone OS', { 'All Others': 'black' })
+
+addDonutChart('#zonePUD', [
+  ['Mugar Family', 1.0],
+  ['All Others', 0.0]
+], 'Zone PUD', { 'All Others': 'black' })
+
+addDonutChart('#zoneR0', [
+  ['WINCHESTER COUNTRY CLUB', 0.237],
+  ['Paul Caruso', 0.0094],
+  ['ARMENIAN CULTURAL', 0.0093],
+  ['HOLMES JOHN K', 0.0065],
+  ['BRUNO RAFFAELE & SILVANA', 0.0063],
+  ['GOODWIN NANCY C/TRUSTEE', 0.0052],
+  ['BOWEN RODERIC/BARBARA', 0.0047],
+  ['PIAZZA LOUISE', 0.0046],
+  ['SANTINI FABRIZIO', 0.0046],
+  ['KOLLIGIAN JOAN E', 0.0043],
+  ['VAN THONG JEAN-MANUEL', 0.0042],
+  ['All Others', 0.7039]
+], 'Zone R0', { 'All Others': 'black' })
+
+addDonutChart('#zoneR1', [
+  ['Town Of Arlington', 0.1635],
+  ['ROMAN CATHOLIC ARCH OF BOS', 0.0119],
+  ['CATHOLIC CEMETARY ASSOC', 0.0101],
+  ['BELMONT COUNTRY CLUB INC', 0.0077],
+  ['THE GREEK ORTHODOX CHURCH', 0.0023],
+  ['STERLING KATHARINE PARKS', 0.0015],
+  ['ARLINGTON LAND TRUST/ INC', 0.0014],
+  ['BOSTON GAS COMPANY', 0.0013],
+  ['KELWYN MANOR ASSOCIATION IN', 0.0012],
+  ['GERMAINE LAWRENCE INCORPORATED', 0.0012],
+  ['SCHOOLS FOR CHILDREN INC', 0.0012],
+  ['All Others', 0.7967]
+], 'Zone R1', { 'All Others': 'black' })
+
+addDonutChart('#zoneR2', [
+  ['ARLINGTON HOUSING AUTHORITY', 0.0129],
+  ['Town Of Arlington', 0.0108],
+  ['ROMAN CATHOLIC ARCH OF BOS', 0.0069],
+  ['GALVIN SEAN D/TRUSTEE', 0.0053],
+  ['HOUSING CORP OF ARLINGTON', 0.0039],
+  ['MORRISSETTE LAURA--ETAL', 0.0035],
+  ['Henry E. Davidson, Jr.', 0.0034],
+  ['PARK AVE HEALTH CENTER', 0.0029],
+  ['R A JOHNSON REALTY INC', 0.0029],
+  ['LOMBARDI KEITH E', 0.0019],
+  ['GALVIN SEAN D TRUSTEE', 0.0019],
+  ['All Others', 0.9437]
+], 'Zone R2', { 'All Others': 'black' })
+
+addDonutChart('#zoneR3', [
+  ['GOSHDIGIAN REALTY LLC', 0.0987],
+  ['POLITANO FRANCESCO', 0.0482],
+  ['FOLEY CAROLE A &', 0.0439],
+  ['A & B MARBLE DESIGN', 0.0417],
+  ['GEANNARIS ANTHONY', 0.0351],
+  ['WEBER MARK A', 0.0329],
+  ['GRASSIA MARK A', 0.0285],
+  ['DILLON JEANNE', 0.0285],
+  ['KLEUTSCH LAUREN', 0.0285],
+  ['65 BROADWAY LLC', 0.0263],
+  ['MELKI SAMIR & RANIA', 0.0263],
+  ['All Others', 0.5614]
+], 'Zone R3', { 'All Others': 'black' })
+
+addDonutChart('#zoneR4', [
+  ['COYNER KARL B', 0.0612],
+  ['THE BOSTON CHURCH OF CHRIST', 0.0583],
+  ['119 PLEASANT STREET LLC', 0.0563],
+  ['CALVERT/ELLENHORN/PRAKASH LLC', 0.0505],
+  ['COLUMBUS CLUB INC', 0.0476],
+  ['118 PLEASANT ST REALTY LLC', 0.0447],
+  ['PRIVITERA PHILIP J/TR', 0.0408],
+  ['KAPLAN ANNETTE TR', 0.0408],
+  ['FREDIEU ROBERT L/ETAL', 0.0359],
+  ['MCMAHON AMBER & JOHN', 0.0291],
+  ['PENG XIAOJUN', 0.0282],
+  ['All Others', 0.5066]
+], 'Zone R4', { 'All Others': 'black' })
+
+addDonutChart('#zoneR5', [
+  ['ARLINGTON HOUSING AUTHORITY', 0.209],
+  ['CONSERVATION FOOD & HEALTH', 0.1573],
+  ['JOHNSON ARTHUR W TR', 0.0737],
+  ['NOSTALGIA PROPERTIES LLC', 0.0713],
+  ['MBC REALTY LLC', 0.0636],
+  ['JOHNSON SANDRA A-TR--ETAL', 0.0485],
+  ['CAMPOBASSO PROPERTIES LLC', 0.036],
+  ['ARLINGTON-GROVE REALTY LLC', 0.0241],
+  ['ARLINGTON-FREMONT RLTY INC', 0.0235],
+  ['JOHNSON SANDRA A--ETAL', 0.022],
+  ['275 BROADWAY LLC', 0.0199],
+  ['All Others', 0.2511]
+], 'Zone R5', { 'All Others': 'black' })
+
+addDonutChart('#zoneR6', [
+  ['ARLINGTON HOUSING AUTHORITY', 0.2129],
+  ['Henry E. Davidson, Jr.', 0.0664],
+  ['NOSTALGIA PROPERTIES LLC', 0.0522],
+  ['GENERATIONS PROPERTIES', 0.0465],
+  ['JOHNSON LIMITED PARTNERSHIP', 0.0394],
+  ['HIGHLAND COURT REALTY LLC', 0.0313],
+  ['HCA CAPITOL SQUARE APARTMENTS', 0.0313],
+  ['JBBP MASS AVE LLC', 0.0308],
+  ['MVC PROPERTY 1 LLC', 0.0247],
+  ['R A JOHNSON REALTY INC', 0.0213],
+  ['SULLIVAN WILLIAM H TRS-ETAL', 0.0209],
+  ['All Others', 0.4223]
+], 'Zone R6', { 'All Others': 'black' })
+
+addDonutChart('#zoneR7', [
+  ['Town Of Arlington', 0.234],
+  ['ARLINGTON HOUSING AUTHORITY', 0.2241],
+  ['MILLBROOK SQ APARTMENTS CO', 0.1723],
+  ['SUNRISE ASSISTED LIVING INC', 0.139],
+  ['HOUSING CORP OF ARLINGTON', 0.0624],
+  ['NOSTALGIA PROPERTIES LLC', 0.0454],
+  ['CEDAR CREST ARLINGTON LLC', 0.0348],
+  ['GALVIN SEAN D /TRUSTEE', 0.0184],
+  ['SCHWAMB MILL PRESERVATION', 0.0149],
+  ['PIECHOTA DENNIS V--ETAL', 0.0113],
+  ['CARNEY JOHN A', 0.0092],
+  ['All Others', 0.0342]
+], 'Zone R7', { 'All Others': 'black' })
