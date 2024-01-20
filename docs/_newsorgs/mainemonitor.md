8bdea84c0d9cf07a28a5f694983c0695e669c8df
Shane Curcuru <asf@shanecurcuru.org>
2024-01-20 12:38:30 -0500

    Add condense functions and start scanning more sites

---
+++ docs/_newsorgs/mainemonitor.md
@@ -0,0 +1,31 @@
+---
+identifier: mainemonitor
+commonName: The Maine Monitor
+legalName: Maine Center for Public Interest Reporting
+description: The Maine Monitor delivers fearless, citizen-supported, nonpartisan journalism that keeps Mainers informed.
+website: https://themainemonitor.org/
+slogan: The Maine Monitor: A nonprofit digital newsroom
+masthead:
+location: Hallowell, ME
+boardSize: '8'
+boardType:
+membershipType:
+boardurl: https://themainemonitor.org/board-of-directors-2/
+bylawsurl:
+numberOfEmployees:
+taxID: 27-2623867
+taxIDLocal: 20100365ND
+taxIDState: ME
+nonprofitStatus: Nonprofit501c3
+budgeturl:
+budgetUsd:
+budgetYear:
+donateurl: https://themainemonitor.giv.sh/20d6
+sponsorurl: https://themainemonitor.org/sponsors/
+advertising:
+policyurl: https://themainemonitor.org/ethics-and-conflicts-of-interest-policies/
+telephone:
+emailContact:
+websitecms: WP
+---
+
--- src/npdetector.rb
+++ src/npdetector.rb
@@ -10,7 +10,9 @@ module NPDetectpr
   require 'nokogiri'
   require 'open-uri'
   require 'json'
+  require 'yaml'
   require 'optparse'
+  require 'set'
 
   MODULE_LOG = [] # Simplistic logging if needed
   ### Other parsing ideas
@@ -18,7 +20,175 @@ module NPDetectpr
   # link rel="https://api.w.org/" signals Wordpress hosting?
   # link rel="amphtml" provides AMP pages for Google?
   # Wordpress theme name: links like: (MIT) ${url}/wp-content/themes/[a-zA-z-]*/style.css
-  TEST_SITES = %w[https://ctmirror.org/  https://FigCityNews.com/ https://hartfordtimes.com/ https://indepthnh.org/ https://nancyonnorwalk.com/ https://newhampshirebulletin.com/ https://newhavenindependent.org/ https://pinetreewatch.org/ https://pvdeye.org/ https://tewksburycarnation.org/ https://theconcordbridge.org/ https://thelocalne.ws/ https://vtdigger.org/ https://www.amjamboafrica.com/ https://www.charlottenewsvt.org/ https://www.commonsnews.org/ https://www.CTMirror.org/ https://www.ecoRI.org/ https://www.ipswichlocalnews.com/ https://www.lexobserver.org/ https://www.oceanstatestories.org/ https://www.themainemonitor.org/ https://www.waterburyroundabout.org/ https://www.yourarlington.com/]
+  # Many sites use yoast: <script type="application/ld+json" class="yoast-schema-graph">...
+  TEST_SITES_X = %w[ https://FigCityNews.com/ ]
+  TEST_SITES_Y = %w[ https://ctmirror.org/  https://FigCityNews.com/ https://hartfordtimes.com/ https://indepthnh.org/ https://nancyonnorwalk.com/ https://newhampshirebulletin.com/ https://newhavenindependent.org/ https://pinetreewatch.org/ https://pvdeye.org/ https://tewksburycarnation.org/ https://theconcordbridge.org/ https://thelocalnews.news/ https://vtdigger.org/ https://www.amjamboafrica.com/ https://www.charlottenewsvt.org/ https://www.commonsnews.org/ https://www.CTMirror.org/ https://www.ecoRI.org/ https://www.ipswichlocalnews.com/ https://www.lexobserver.org/ https://www.oceanstatestories.org/ https://www.themainemonitor.org/ https://www.waterburyroundabout.org/ https://www.yourarlington.com/]
+  TEST_SITES = %w[
+    https://avlwatchdog.org/
+https://aspenjournalism.org
+https://www.baltimorebrew.com
+https://shastascout.org/
+https://beltmag.com
+https://austonia.com
+https://carolinapublicpress.org
+https://nowkalamazoo.com
+https://chicosol.org
+https://www.civicstory.org
+https://www.coastalreview.org
+https://codastory.com
+https://dcist.com
+https://eastlansinginfo.news
+https://www.bayoubrief.com
+https://www.floridabulldog.org
+https://www.greensourcedfw.org
+https://www.hcn.org
+https://www.thedailycatch.org
+https://marylandreporter.com
+https://matternews.org
+https://mountainjournal.org
+https://www.midstory.org
+https://minnpost.com
+https://Mississippitoday.org
+https://montanafreepress.org
+https://www.mymcmedia.org
+https://www.njspotlight.com
+https://oklahomawatch.org
+https://voiceofoc.org
+https://www.Calo.org
+https://www.searchlightnm.org
+https://www.TucsonSentinel.com
+https://missionlocal.org
+https://www.sdnewswatch.org
+https://tarbell.org
+https://boulderreportinglab.org
+https://theaustinbulldog.org
+https://latinorebels.com
+https://boyleheightsbeat.com
+https://breckenridgetexan.com
+https://thecity.nyc
+https://www.thecurrentla.com
+https://www.daggerpress.com
+https://thedcline.org
+https://thefern.org/donate
+https://kycir.org
+https://thelensnola.org
+https://www.thelundreport.org/
+https://investigatemidwest.org
+https://thetylerloop.com
+https://nkytribune.com
+https://wisconsinwatch.org
+https://nuvo.net
+https://billypenn.com
+https://chicagoreporter.com
+https://therivardreport.com
+https://rochesterbeacon.com
+https://sammamish.news
+https://sierranevadaally.org
+https://southsideweekly.com
+https://utahinvestigative.org
+https://www.localnewsmatters.org
+https://www.tonemadison.com
+https://iowacapitaldispatch.com/
+https://voicesofmontereybay.org
+https://thenevadaindependent.com
+https://www.spotlightpa.org
+https://www.marylandmatters.org
+https://witnessla.com
+https://www.wyofile.com
+https://www.inewsource.org
+https://SanJoseInside.com
+https://xtown.la
+https://eltimpano.org
+https://berkeleyside.com
+https://coloradonewsline.com/
+https://coloradofoic.org/
+https://ljidelaware.org
+https://jaxtoday.org
+https://verocommunique.com
+https://thecurrentga.org
+https://austintalks.org/
+https://hoptownchronicle.org/
+https://eplocalnews.org
+https://webbcity.net
+https://thebeacon.media
+https://thelocalreporter.press
+https://NorthCarolinaHealthNews.org
+https://nmindepth.com
+https://chalkbeat.org
+https://documentedny.com/about-us
+https://www.nysfocus.com
+https://www.austinmonitor.com
+https://theaustinbulldog.org
+https://www.virginiamercury.com
+https://cardinalnews.org
+https://www.gigharbornow.org
+https://www.spokanefavs.com
+https://www.wausaupilotandreview.com
+https://www.mountainstatespotlight.org
+https://mountainstatespotlight.org/
+https://www.cinespeak.org/
+https://azluminaria.org
+https://grist.org
+https://www.conectaarizona.com
+https://www.catchlight.io
+https://fresnoland.org
+https://openvallejo.org
+https://RiversideRecord.org
+https://edsource.org
+https://lookoutlocal.com
+https://www.afrolanews.org
+https://www.berkeleyside.com
+https://spotlightschools.com
+https://thefrisc.com
+https://westsideobserver.com
+https://theyappie.com
+https://www.themarjorie.org
+https://www.oviedocommunitynews.org
+https://jaxtrib.org
+https://www.theatlantavoice.com
+https://canopyatlanta.org
+https://macon-newsroom.com
+https://www.objectivejournalism.org
+https://www.therecordnorthshore.org
+https://illinoisanswers.org
+https://harveyworld.org
+https://civiclex.org
+https://thebaltimorebanner.com
+https://cecil.tv
+https://tostadamagazine.com
+https://theuptake.org
+https://join.theoptimist.mn
+https://www.kansascitydefender.com
+https://www.mississippifreepress.org
+https://ashevillefreepress.com
+https://nebraskajournalismtrust.org
+https://jerseybee.org
+https://www.montclairlocal.news
+https://atlanticcityfocus.tinynewsco.org
+https://bloomfieldinfo.org
+https://www.publicsq.org
+https://questanews.com
+https://www.doublescoop.art
+https://thedailycatch.org
+https://nyc.streetsblog.org
+https://www.TheBuckeyeFlame.com
+https://eyeonohio.com
+https://www.thelandcle.org
+https://www.Blackgirlincle.com
+https://www.thereportingproject.org
+https://www.bigiftrue.org
+https://ashland.news
+https://www.underscore.news
+https://www.tubecityonline.com
+https://www.thegburgvoice.org
+https://www.thegreencities.com
+https://elpasomatters.org
+https://www.islandmatters.org/
+https://nowcastsa.com
+https://www.texasobserver.org
+https://www.austinvida.com
+https://tumbleweird.org
+  ]
   CSS_MAP = {
     'slogan' => '.site-description',
     'copyright' => '.copyright',
@@ -34,6 +204,9 @@ module NPDetectpr
   }
   METAS = 'metas'
   LINKS = 'links'
+  NAVLINKS = 'linksnav'
+  FOOTERLINKS = 'linksfooter'
+  ALLLINKS = 'alllinks'
   ABOUT = 'aboutlinks'
   DONATE = 'donatelinks'
   LINKRX_MAP = {
@@ -79,7 +252,7 @@ module NPDetectpr
     return metas
   end
 
-  # @return hash of various potentially interesting unique links
+  # @return hash of various potentially interesting unique link hrefs
   def get_links(nodelist, lookfor)
     tmp = Hash.new { | h, k | h[k] = [] }
     nodelist.each do | node | # FIXME horribly inefficient
@@ -97,9 +270,17 @@ module NPDetectpr
     return links
   end
 
-  # @return array of text nodes that match regex
-  def get_text_matches(nodelist, rx)
-    return nodelist.map(&:content).select { | t | rx.match(t) }
+  # @return hash of interesting link hrefs, plus all link contents
+  def get_links_all(nodelist, lookfor)
+    links = get_links(nodelist, lookfor)
+    links[ALLLINKS] = []
+    nodelist.map(&:content).uniq.each do | txt |
+      if txt
+        t = txt.strip
+        links[ALLLINKS] << t if t.length > 3 # Arbitrary; ignore non-word links
+      end
+    end
+    return links
   end
 
   # @return hash of arrays of text nodes matching our maps
@@ -122,6 +303,8 @@ module NPDetectpr
       end
       data[METAS] = get_metas(doc.xpath('/html/head'))
       data[LINKS] = get_links(doc.css('a'), LINKRX_MAP)
+      data[NAVLINKS] = get_links_all(doc.css('nav a'), LINKRX_MAP)
+      data[FOOTERLINKS] = get_links_all(doc.css('footer a'), LINKRX_MAP)
       data[TEXT_MATCHES] = scan_text(doc.xpath('/html/body//text()'), TEXTRX_MAP)
       if data[TEXT_MATCHES][EIN_SCAN].empty?
         # If we didn't find any EINs, then also parse about pages to scan text
@@ -162,7 +345,7 @@ module NPDetectpr
       itemtypes = body.xpath('//*[@itemtype]') # Does site use schema.org metadata?
       data['itemtypes'] = itemtypes.children.length unless itemtypes.children.empty?
     rescue StandardError => e
-      MODULE_LOG << "error_parse_newsurl: on #{siteurl}"
+      MODULE_LOG << "error_parse_newsurl(#{siteurl}): #{e.message}"
       data['error_parse_newsurl'] = "#{e.message}\n\n#{e.backtrace.join("\n\t")}"
     end
     return data
@@ -200,18 +383,75 @@ module NPDetectpr
     File.open("#{fileroot}.json", 'w') do |f|
       f.puts JSON.pretty_generate(data)
     end
+    condense_site(fileroot)
+  end
+
+  # Convenience method to condense site data and aggregate similar data
+  def condense_site(fileroot, aggregate)
+    begin
+      data = JSON.load_file("#{fileroot}.json")
+      condensed = {}
+      condensed['title'] = data[METAS].fetch('title', nil)
+      condensed['title'] = data[METAS].fetch('titleog', '') unless condensed['title']
+      condensed['description'] = data[METAS].fetch('description', nil)
+      condensed['description'] = data[METAS].fetch('descriptionog', '') unless condensed['description']
+      condensed['generator'] = data[METAS].fetch('generator', []).to_s
+      eins = []
+      nonprofit = []
+      data['textmatch'].each do | k, v | # HACK need to figure out what we really need here
+        tmp = {}
+        next if 'nonprofit'.eql?(k)
+        if '501c3'.eql?(k)
+          nonprofit.concat(v)
+        else
+          if EIN_SCAN.eql?(k)
+            tmp = v
+          else
+            tmp = v[EIN_SCAN]
+          end
+          eins.concat(tmp) unless tmp.empty?
+          end
+      end
+      condensed[EIN_SCAN] = eins
+      condensed['nonprofit'] = nonprofit
+      MODULE_LOG << "condense_site(#{fileroot}): found EIN" unless eins.empty?
+      File.open("#{fileroot}.yml", 'w') do |f|
+        f.puts condensed.to_yaml(:stringify_names => true)
+      end
+      aggregate[NAVLINKS].merge(data[NAVLINKS][ALLLINKS]) if data[NAVLINKS].fetch(ALLLINKS, nil)
+      aggregate[FOOTERLINKS].merge(data[FOOTERLINKS][ALLLINKS]) if data[FOOTERLINKS].fetch(ALLLINKS, nil)
+
+    rescue StandardError => e
+      MODULE_LOG << "condense_site(#{fileroot}): #{e.message}\n\n#{e.backtrace.join("\n\t")}"
+    end
   end
 
   # ### #### ##### ######
   # Main method for command line use
-  if __FILE__ == $PROGRAM_NAME
-    dir = 'tmp'
+  def process_test(dir)
     Dir.mkdir(dir) unless Dir.exist?(dir)
-    # u = ARGV.first
-    # f = File.join(dir, url2file(u))
     TEST_SITES.each do | siteurl |
       process_site(siteurl, dir)
     end
+  end
+  def process_condense(dir)
+    data = {} # Aggregate lists of common link names
+    data[NAVLINKS] = Set.new()
+    data[FOOTERLINKS] = Set.new()
+    Dir["#{dir}/*.json"].each do |f|
+      condense_site(f.sub('.json', ''), data)
+    end
+    data[NAVLINKS] = data[NAVLINKS].to_a.sort
+    data[FOOTERLINKS] = data[FOOTERLINKS].to_a.sort
+    File.open("npdetector.json", 'w') do |f|
+        f.puts JSON.pretty_generate(data)
+      end
+  end
+  if __FILE__ == $PROGRAM_NAME
+    dir = 'tmp'
+    # process_test(dir)
+    # condense_site(...)
+    process_condense(dir)
     puts "---- done; log of warnings:"
     puts JSON.pretty_generate(MODULE_LOG)
   end
